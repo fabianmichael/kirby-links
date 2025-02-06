2808e6792072e76f8d1fe7d36afc772a5426bcf5
Fabian Michael <hallo@fabianmichael.de>
2025-02-06 19:50:40 +0100

    feat: register blueprints under a second name

--- index.php
+++ index.php
@@ -13,6 +13,14 @@ Kirby::plugin('fabianmichael/links', [
 		'fields/link' => __DIR__ . '/blueprints/fields/link.yml',
 		'fields/links' => __DIR__ . '/blueprints/fields/links.yml',
 		'fields/navigation' => __DIR__ . '/blueprints/fields/navigation.yml',
+		
+		'links/blocks/link' => __DIR__ . '/blueprints/blocks/link.yml',
+		'links/blocks/menu-link' => __DIR__ . '/blueprints/blocks/menu-link.yml',
+		'links/blocks/menu-submenu' => __DIR__ . '/blueprints/blocks/menu-submenu.yml',
+		'links/fields/link-group' => __DIR__ . '/blueprints/fields/link-group.yml',
+		'links/fields/link' => __DIR__ . '/blueprints/fields/link.yml',
+		'links/fields/links' => __DIR__ . '/blueprints/fields/links.yml',
+		'links/fields/navigation' => __DIR__ . '/blueprints/fields/navigation.yml',
 	],
 	'blocksMethods' => [
 		'hasValidLinks' => function (): bool
